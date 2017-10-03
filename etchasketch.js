c5571d919ebdab018c229f9f1048218491ce7ab8
Brandon B <bmbost1983@gmail.com>
2017-10-03 11:30:31 -0500

    Add files via upload
    Added etchasketch.js and index.html

---
+++ etchasketch.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+    $("#body").append("<div id='dataContainer'></div>");
+    $("#dataContainer").css({"float": "left"});
+    $("#btnReset").css({"margin": "10px"});
+    FillBody(16, 16);
+
+    $("#btnReset").on("click", function() {
+        var input = prompt("Please enter the size grid:", "16");
+        
+        if (input != null) {
+            FillBody(parseInt(input, 10), parseInt(input, 10));
+        }
+    });
+});
+
+function FillBody(columns, rows) {
+    var html = "<table id='tblContainer'>";
+    for (var i = 0; i < rows; i++) {
+        html += "<tr>";
+        for (var j = 0; j < columns; j++) {
+            html += "<td><div class='container'></div></td>";
+        }
+        html += "</tr>";
+    }
+    html += "</table>";
+
+    $("#dataContainer").html(html);
+    $(".container").css({"border-color": "#000",
+                         "border-width": "1px",
+                         "border-style": "solid",
+                         "width": "100%",
+                         "height": "100%"});
+
+    $("#tblContainer").css({"width": "960px",
+                            "height": "960px"});
+
+    $(".container").hover(
+        function() {
+            $(this).css({"background-color": "blue"});
+        }, function() {
+        }
+    );
+}
+
